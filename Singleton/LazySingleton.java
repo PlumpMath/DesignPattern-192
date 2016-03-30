522a031625985bd4e5880d718b63b48a474e0011
Fei Chen <sdcf2010@gmail.com>
2016-03-30 22:24:32 +0100

    Volatile keyword explainition

--- Singleton/LazySingleton.java
+++ Singleton/LazySingleton.java
@@ -2,6 +2,10 @@ public class LazySingleton {
 
     private volatile static LazySingleton instance;
 
+    // Note: It is not thread-safe if not using the 'volatile' keyword. The problem is a thread could read a partially constructed instance, while another thread is constructing the instance. It is because of the reordering of cpu instructions, there could be a chance that the writing thread set the reference before initiating the new instance.
+    // volatile guarantees the write happens before the read.
+    // See section 16.2.4 of the "Java Concurrency In Practice" book.
+
     private LazySingleton() {
     }
 
